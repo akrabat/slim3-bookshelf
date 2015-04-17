773a64ce2ad814fd3c0ffe1161ff3aa5e324633d
Rob Allen <rob@akrabat.com>
2015-04-17 18:44:28 +0100

    SetName() => setName()

--- app/src/routes.php
+++ app/src/routes.php
@@ -1,6 +1,6 @@
 <?php
 // Route configuration
 
-$app->get('/', 'Bookshelf\AuthorController:listAuthors')->SetName('list-authors');
-$app->get('/authors/{author_id:[0-9]+}', 'Bookshelf\AuthorController:listBooks')->SetName('author');
-$app->get('/books', 'Bookshelf\BookController:listBooks')->SetName('list-books');
+$app->get('/', 'Bookshelf\AuthorController:listAuthors')->setName('list-authors');
+$app->get('/authors/{author_id:[0-9]+}', 'Bookshelf\AuthorController:listBooks')->setName('author');
+$app->get('/books', 'Bookshelf\BookController:listBooks')->setName('list-books');
