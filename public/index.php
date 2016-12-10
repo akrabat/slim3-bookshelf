d9eb678251a590aa919a77c3345d88ce413bd3f3
Rob Allen <rob@akrabat.com>
2016-12-10 21:39:24 +0000

    Serve static paths for cli-server

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
 // All file paths relative to root
 chdir(dirname(__DIR__));
 
